a8e0b909e4af9adb6e55c718aa821779ef120ff2
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-10 17:52:27 +0900

    division計算結果に.to_f処理の追加

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,16 +3,15 @@ class CalcsController < ApplicationController
     int_first = params[:int_first].to_i
     arithmetic = params[:arithmetic]
     int_second = params[:int_second].to_i
-    case arithmetic
-    when "addition" then
-      @answer = int_first + int_second
-    when "subtraction" then
-      @answer = int_first - int_second
-    when "multiplication" then
-      @answer = int_first * int_second
-    when "division" then
-      @answer = int_first / int_second
-    else
-    end
+    @answer = case arithmetic
+              when "addition" then
+                int_first + int_second
+              when "subtraction" then
+                int_first - int_second
+              when "multiplication" then
+                int_first * int_second
+              when "division" then
+                int_first / int_second.to_f
+              end
   end
 end
